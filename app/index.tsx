56f7166a6220b0fd7750a42ea1f507bbf47e80a6
Nelson Khor <cnkhor92@gmail.com>
2024-12-07 00:29:53 +0800

    Optimize code

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Alert, Button, Text, View, Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Link, router } from 'expo-router';
 import * as LocalAuthentication from "expo-local-authentication"
@@ -7,26 +7,36 @@ import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function App() {
   const [isBiometricSupported, setIsBiometricSupported] = useState(false);
+  console.log(isBiometricSupported)
 
   async function loginAuth() {
-    const getEnrolledAuth = await LocalAuthentication.getEnrolledLevelAsync()
-    const hasHardwareAuth = await LocalAuthentication.hasHardwareAsync()
-    const isEnrolledAuth = await LocalAuthentication.isEnrolledAsync()
-    const supportedAuth = await LocalAuthentication.supportedAuthenticationTypesAsync()
+    try {
+      const hasHardwareAuth = await LocalAuthentication.hasHardwareAsync()
+      const isEnrolledAuth = await LocalAuthentication.isEnrolledAsync()
+      const supportedAuth = await LocalAuthentication.supportedAuthenticationTypesAsync()
 
-    console.log('Supported: ', supportedAuth) // [1] - fingerprint
-    console.log({getEnrolledAuth, hasHardwareAuth, isEnrolledAuth}) // 3, true, true
+      setIsBiometricSupported(supportedAuth.length > 0)
+      
 
-    if (hasHardwareAuth && isEnrolledAuth) {
-      const respond = await LocalAuthentication.authenticateAsync();
-      console.log(respond);
-
-      if (respond.success) {
-        router.push('/home');
+      if (hasHardwareAuth && isEnrolledAuth) {
+        const respond = await LocalAuthentication.authenticateAsync();
+        if (respond.success) {
+          router.push('/home');
+        }
+      } else {
+        Alert.alert(
+          'Authentication Error',
+          'Biometric authentication is not supported or enrolled on this device.',
+          [{ text: 'OK' }]
+        );
       }
-    } else {
-      console.log('Biometric authentication is not supported or enrolled.');
+    } catch (error) {
+      console.log('Authentication error:', error)
     }
+  }
+
+  function navigateToHome(): void {
+    router.push('/home')
   }
 
   useEffect(() => {
@@ -36,12 +46,12 @@ export default function App() {
   return (
     <>
       <View className="flex-1 items-center justify-center bg-white" >
-        <Text className="text-2xl font-robotoRegular">Login with Biometric</Text>
+        <Text className="text-2xl font-robotoRegular w-4/5 text-center">Press the icon to login with Biometric Fingerprint</Text>
+        <FontAwesome5 name="fingerprint" size={36} color="black" className="m-5" onPress={loginAuth}/>
+        {!isBiometricSupported && (
+          <Button title='Or login here for web browser' onPress={navigateToHome} />
+        )}
         <StatusBar style="auto"></StatusBar>
-        <Link href="/home" style={{ color: 'blue' }}>
-          Login to Home
-        </Link>
-        <FontAwesome5 name="fingerprint" size={36} color="black" className="pt-5" onPress={loginAuth()}/>
       </View>
     </>
   );
