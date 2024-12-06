af1605dee5136045bd733fa2f39d48e04c3952f7
Nelson Khor <cnkhor92@gmail.com>
2024-12-06 22:58:29 +0800

    Add auth function

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 import * as LocalAuthentication from "expo-local-authentication"
 import { useEffect, useState } from 'react';
 import { FontAwesome5 } from '@expo/vector-icons';
@@ -14,17 +14,28 @@ export default function App() {
     const isEnrolledAuth = await LocalAuthentication.isEnrolledAsync()
     const supportedAuth = await LocalAuthentication.supportedAuthenticationTypesAsync()
 
-    console.log('Supported: ', supportedAuth)
-    console.log({getEnrolledAuth, hasHardwareAuth, isEnrolledAuth})
+    console.log('Supported: ', supportedAuth) // [1] - fingerprint
+    console.log({getEnrolledAuth, hasHardwareAuth, isEnrolledAuth}) // 3, true, true
+
+    if (hasHardwareAuth && isEnrolledAuth) {
+      const respond = await LocalAuthentication.authenticateAsync();
+      console.log(respond);
+
+      if (respond.success) {
+        router.push('/home');
+      }
+    } else {
+      console.log('Biometric authentication is not supported or enrolled.');
+    }
   }
 
   useEffect(() => {
     loginAuth();
-  })
+  }, [])
 
   return (
     <>
-      <View className="flex-1 items-center justify-center bg-white">
+      <View className="flex-1 items-center justify-center bg-white" >
         <Text className="text-2xl font-robotoRegular">Login with Biometric</Text>
         <StatusBar style="auto"></StatusBar>
         <Link href="/home" style={{ color: 'blue' }}>
