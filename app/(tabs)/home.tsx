d41cebd3fc39248a2e36a3702003c67894402085
Nelson Khor <cnkhor92@gmail.com>
2024-12-08 01:42:09 +0800

    Add authorization for viwing amount

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,39 +1,57 @@
-import { View, Text, FlatList } from 'react-native'
-import React, { ReactElement } from 'react'
+import { View, Text, FlatList, RefreshControl } from 'react-native'
+import React, { ReactElement, useCallback, useState } from 'react'
 import TransactionItem from '../components/TransactionItem'
 import { TransactionType } from '../components/models'
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import mockData from '../data/mockData.json'
-import { TransactionItemProps } from "../components/TransactionItem"
+import useStore from '../store/useStore';
+import useAuthorization from '../hooks/useAuthorization';
 
 
-export default function Home() {
+export default function Home(): ReactElement {
+  const [ isRefreshing, setIsRefreshing ] = useState<boolean>(false)
+  const isAuthorized = useStore((state) => state.isAuthorized)
+  const { onAuthorized, onUnauthorized } = useAuthorization()
+
+  const onRefresh = useCallback(() => {
+    setIsRefreshing(true);
+    // mock fetching data for 2 sec
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 2000);
+  }, []);
+
   return (
-    <FlatList
-      data={mockData}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={(transaction) => 
-        <TransactionItem     
-          id={transaction.item.id} 
-          amount={transaction.item.amount} 
-          date={transaction.item.date} 
-          description={transaction.item.description}
-          type={transaction.item.type as TransactionType}
-        />
-      }
-    />
-    // <View>
-    //   <Text>Home</Text>
-    //   {
-    //     mockData.map((transaction) => (
-    //       <TransactionItem 
-    //         id={transaction.id} 
-    //         amount={transaction.amount} 
-    //         date={transaction.date} 
-    //         description={transaction.description} 
-    //         type={transaction.type as TransactionType}
-    //       />
-    //     ))
-    //   }
-    // </View>
+    <>
+      <View className='mx-2 my-6'>
+        <Text className='font-robotoBold text-4xl'>
+          Welcome back!
+        </Text>
+        <Text className='font-robotoRegular text-xl'>
+          You can view your transaction history below.
+        </Text>
+        <View className='m-2'>
+        {isAuthorized 
+          ? <FontAwesome5 name="eye" size={24} color="black" onPress={onAuthorized} /> 
+          : <FontAwesome5 name="eye-slash" size={24} color="black" onPress={onUnauthorized}/> }
+        </View>
+      </View>
+      <FlatList
+        data={mockData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={(transaction) => 
+          <TransactionItem     
+            id={transaction.item.id} 
+            amount={transaction.item.amount} 
+            date={transaction.item.date} 
+            description={transaction.item.description}
+            type={transaction.item.type as TransactionType}
+          />
+        }
+        refreshControl={
+          <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh}/>
+        }
+      />
+    </>
   )
 }
--- app/components/TransactionItem.tsx
+++ app/components/TransactionItem.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React, { ReactElement } from 'react'
 import { TransactionType } from './models'
+import useStore from '../store/useStore'
 
 export interface TransactionItemProps {
     id: number
@@ -13,6 +14,8 @@ export interface TransactionItemProps {
 const cardStyle = "border-solid rounded-md bg-primaryBlue mx-2 my-4 shadow-md opacity-1"
 
 export default function TransactionItem({id, amount, date, description, type}: TransactionItemProps): ReactElement {
+  const isAuthorized = useStore((state) => state.isAuthorized)
+
   return (
     <View className={cardStyle}>
         <View className="flex-row justify-between m-2">
@@ -20,7 +23,7 @@ export default function TransactionItem({id, amount, date, description, type}: T
             <Text>{date}</Text>
         </View>
         <View className="align-middle m-2">
-            <Text className="font-bold text-lg">RM {amount.toFixed(2)}</Text>
+            <Text className="font-bold text-lg">RM {isAuthorized ? amount.toFixed(2) : '*****'}</Text>
         </View>
 
         <View className="flex-row justify-between m-2">
