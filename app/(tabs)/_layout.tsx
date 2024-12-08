437712695c10a8a1e5f366c277b68352f78316c4
Nelson Khor <cnkhor92@gmail.com>
2024-12-08 18:24:12 +0800

    Prevent accidental back button

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,89 +1,105 @@
-import { View, Text, Image, ImageSourcePropType, ImageResizeMode, BackHandler } from 'react-native'
-import React, { useCallback } from 'react'
-import { Tabs, Redirect, useFocusEffect } from 'expo-router'
-import icons from '../../assets/icons/icons'
-
-export interface TabIconProps {
-    icon: string | ImageSourcePropType
-    color: string
-    name: string
-    isFocused: boolean
-}
-
-function useBackHandler() {
-    useFocusEffect(
-        useCallback(() => {
-            const onBackPress = () => {
-                return true;
-            };
-            BackHandler.addEventListener('hardwareBackPress', onBackPress);
-            return () => BackHandler.removeEventListener('hardwareBackPress', onBackPress);
-        }, [])
-    );
-}
-
-function TabIcon({ icon, color, name, isFocused }: TabIconProps) {
-    const source = typeof icon === 'string' ? { uri: icon } : icon
-
+import {
+    View,
+    Text,
+    Image,
+    ImageSourcePropType,
+    BackHandler,
+  } from 'react-native';
+  import React, { ReactElement, useCallback } from 'react';
+  import { Tabs, useFocusEffect } from 'expo-router';
+  import icons from '../../assets/icons/icons';
+  import useStore from '../store/useStore';
+  
+  export interface TabIconProps {
+    icon: string | ImageSourcePropType;
+    color: string;
+    name: string;
+    isFocused: boolean;
+  }
+  
+  function TabIcon({ icon, color, name, isFocused }: TabIconProps) {
+    const source = typeof icon === 'string' ? { uri: icon } : icon;
+  
     return (
-        <View className='items-center justify-center gap-2'>
-            <Image
-                source={source}
-                resizeMode='contain'
-                tintColor={color}
-                className='h-6 w-6'
-            />
-            <Text className={`${isFocused ? 'font-robotoBold' : 'font-robotoRegular'} w-20 text-center`}>
-                {name}
-            </Text>
-        </View>
-    )
-}
-
-export default function TabsLayout() {
-    useBackHandler()
-  return (
-    <>
-        <Tabs
-            screenOptions={{
-                tabBarShowLabel: false,
-                tabBarStyle: {
-                    height: 70,
-                    paddingTop: 15,
-                }
-            }}
+      <View className="items-center justify-center gap-2">
+        <Image
+          source={source}
+          resizeMode="contain"
+          tintColor={color}
+          className="h-6 w-6"
+        />
+        <Text
+          className={`${isFocused ? 'font-robotoBold' : 'font-robotoRegular'} w-20 text-center`}
         >
-            <Tabs.Screen
-                name="home"
-                options={{
-                    title: 'Home',
-                    headerShown: false,
-                    tabBarIcon: ({ color, focused }) => (
-                        <TabIcon
-                            icon={icons.home}
-                            color={color}
-                            name="Home"
-                            isFocused={focused}
-                        />
-                    )
-                }}
-            />
-            <Tabs.Screen
-                name="setting"
-                options={{
-                    title: 'Home',
-                    headerShown: false,
-                    tabBarIcon: ({ color, focused }) => (
-                        <TabIcon
-                            icon={icons.settings}
-                            color={color}
-                            name="Settings"
-                            isFocused={focused}
-                        />
-                    )
-                }}
-            />
+          {name}
+        </Text>
+      </View>
+    );
+  }
+  
+  export default function TabsLayout(): ReactElement {
+    const isLoggedIn = useStore((state) => state.isLoggedIn);
+  
+    // Prevent back button from logging out
+    useFocusEffect(
+      useCallback(() => {
+        const onBackPress = () => {
+          if (isLoggedIn) {
+            return true;
+          }
+          // If not on home screen, allow back button to work
+          return false;
+        };
+  
+        BackHandler.addEventListener('hardwareBackPress', onBackPress);
+        return () =>
+          BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+      }, [isLoggedIn])
+    );
+  
+    return (
+      <>
+        <Tabs
+          screenOptions={{
+            tabBarShowLabel: false,
+            tabBarStyle: {
+              height: 70,
+              paddingTop: 15,
+            },
+          }}
+        >
+          <Tabs.Screen
+            name="home"
+            options={{
+              title: 'Home',
+              headerShown: false,
+              tabBarIcon: ({ color, focused }) => (
+                <TabIcon
+                  icon={icons.home}
+                  color={color}
+                  name="Home"
+                  isFocused={focused}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="setting"
+            options={{
+              title: 'Home',
+              headerShown: false,
+              tabBarIcon: ({ color, focused }) => (
+                <TabIcon
+                  icon={icons.settings}
+                  color={color}
+                  name="Settings"
+                  isFocused={focused}
+                />
+              ),
+            }}
+          />
         </Tabs>
-    </>
-  )
-}
+      </>
+    );
+  }
+  
